852a963b59d5a7430f78e3d7008e8a0c13edf899
rohitkumar <rohitkumar620200@gmail.com>
2024-08-25 15:31:40 +0530

    Set the routes and updated the Navbar

--- blogging-website-frontend/src/App.jsx
+++ blogging-website-frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
     return (
-        <Navbar />
+        // Nested routes / + signin => signin 
+        <Routes>
+            <Route path="/" element={<Navbar />} >
+                <Route path="signin" element={<h1>Sign in Page</h1>} /> 
+                <Route path="signup" element={<h1>Sign up Page</h1>} /> 
+            </Route>
+        </Routes>
     )
 }
 
